4eeb503f70331cbe64bc518e49ba2d39b15f15ff
fish210 <82754316+fish210@users.noreply.github.com>
2022-06-26 23:32:31 +0800

    Add files via upload

---
+++ create a window x86 ucrt static/create a window x86 ucrt static.cpp
@@ -0,0 +1,26 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include "wx/wx.h"
+
+// use gcc12 uct link
+// winlibs-i686-posix-dwarf-gcc-12.1.0-mingw-w64ucrt-10.0.0
+
+// https://github.com/wxWidgets/wxWidgets/releases/tag/v3.1.7
+// wxWidgets-3.1.7.7z 
+// compile link to "i686 gcc-12.1.0-mingw-w64ucrt"
+
+//自己编译出静态库，或者直接用我编译好的静态库文件 .a文件
+
+class MyApp : public wxApp
+{
+public:
+  virtual bool OnInit();
+};
+IMPLEMENT_APP(MyApp)
+
+bool MyApp::OnInit()
+{
+  wxFrame *frame = new wxFrame(0, wxID_ANY, wxT("Window title"));
+  frame->Show();
+  return true;
+}
